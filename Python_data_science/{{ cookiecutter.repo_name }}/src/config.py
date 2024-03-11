2dcfc287362e08eb7664eeee323c614f26d6974b
reidhin <hansweda@gmail.com>
2024-03-11 19:01:01 +0100

    Adding config.py to Python cookiecutter

---
+++ Python_data_science/{{ cookiecutter.repo_name }}/src/config.py
@@ -0,0 +1,10 @@
+import os
+
+# the root directory of the project
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# The data directory of the project
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+# The data directory of the project
+FIGURE_DIR = os.path.join(ROOT_DIR, "reports", "figures")
